8aec972607d8043335263f733d08e7fa49d1048a
Kangxu Wang <kangxu@umich.edu>
2020-11-29 11:15:26 -0800

    Set up initial package using spring initializr

---
+++ src/main/java/com/bd/xchoice/XChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.bd.xchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/xchoice/XChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.xchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
